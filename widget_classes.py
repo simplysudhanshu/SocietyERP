f5381ea12e8151fb1490e7a026111d7bb0b66e40
sudhanshu <sudhanshu.1.k@gmail.com>
2020-06-22 13:32:51 +0530

    Landing page UI v1.0

--- widget_classes.py
+++ widget_classes.py
@@ -10,15 +10,59 @@ import marathi
 flats = [str(x) for x in range(0, 22)]
 
 
+def init_members_panel():
+    search_button = QPushButton("Add New Member")
+    add_button = QPushButton("Search Member Details")
+
+    members_layout = QVBoxLayout()
+    members_layout.addWidget(search_button)
+    members_layout.addWidget(add_button)
+
+    members_group = QGroupBox("Handling Members :")
+    members_group.setLayout(members_layout)
+    return members_group
+
+
+def init_finance_panel():
+    search_button = QPushButton("Add New Transaction")
+    add_button = QPushButton("Search Records")
+
+    finance_layout = QVBoxLayout()
+    finance_layout.addWidget(search_button)
+    finance_layout.addWidget(add_button)
+
+    finance_group = QGroupBox("Handling finance :")
+    finance_group.setLayout(finance_layout)
+    return finance_group
+
+
+def init_stats_panel():
+    current_month = date.today().strftime('%B')
+
+    current_stats = QLabel(f"Monthly Fee Status ({current_month}) : 10 received, 12 pending.")
+    current_collection = QLabel(f"Monthly Collection ({current_month}) : 1000 INR.")
+    stats_button = QPushButton("Get Detailed Stats")
+
+    stats_layout = QVBoxLayout()
+    stats_layout.addWidget(current_stats)
+    stats_layout.addWidget(current_collection)
+    stats_layout.addWidget(stats_button)
+
+    stats_group = QGroupBox("Statistics :")
+    stats_group.setLayout(stats_layout)
+    return stats_group
+
+
 class home(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.spaceItem = QSpacerItem(150, 60, QSizePolicy.Expanding)
 
-        self.initUI()
+        self.template_UI()
 
-    def initUI(self):
+    def template_UI(self):
         grid = QGridLayout()
+        grid.setSpacing(50)
         self.setLayout(grid)
 
         # --- TITLE
@@ -40,7 +84,7 @@ class home(QWidget):
         sub_title.setStyleSheet("font: bold")
         sub_title.setFixedHeight(30)
 
-        app_name = QLabel("MEMBERSHIP FEE COLLECTION PORTAL.")
+        app_name = QLabel("MEMBERSHIP FEE COLLECTION PORTAL")
         app_name.setFixedHeight(30)
         app_name.setStyleSheet("border: 1px solid black;")
 
@@ -57,13 +101,19 @@ class home(QWidget):
 
         # --- WELCOME
         welcome_layout = QHBoxLayout()
-        welcome = QLabel("Welcome, ")
-        name = QLabel("Mr. X")
+        welcome = QLabel("                                       Hello,")
+        name = QLabel("Mr. X        ")
 
-        welcome_layout.insertStretch(0, 1)
+        welcome.setFixedHeight(100)
+        name.setFixedHeight(100)
+
+        welcome.setAlignment(Qt.AlignTop)
+        name.setAlignment(Qt.AlignTop)
+
+        name.setStyleSheet("font: bold")
+
         welcome_layout.addWidget(welcome)
         welcome_layout.addWidget(name)
-        welcome_layout.insertStretch(3, 1)
 
         # --- DATE
         date_label = QLabel(f"{date.today().strftime('%d %B, %Y')}")
@@ -79,20 +129,17 @@ class home(QWidget):
         # comboBox.setFont(QFont('SansSerif', 10))
         # comboBox.setStyleSheet('text-color: black; selection-background-color: rgb(215,215,215)')
 
-        # --- MEMBERS TAB
-        search_button = QPushButton("Search Records")
-        add_button = QPushButton("Add Member")
-
-        members_layout = QVBoxLayout()
-        members_layout.addWidget(search_button)
-        members_layout.addWidget(add_button)
-
-        members_group = QGroupBox("Handling Members :")
-        members_group.setLayout(members_layout)
+        # --- INIT TABS
+        members_panel = init_members_panel()
+        finance_panel = init_finance_panel()
+        stats_panel = init_stats_panel()
 
         # ------- FINAL GRID
 
-        grid.addWidget(title_group, 0, 0, 1, 4)
-        grid.addLayout(welcome_layout, 1, 0, 1, 3)
-        grid.addWidget(date_label, 1, 3, 1, 1)
-        grid.addWidget(members_group, 2, 0)
+        grid.addWidget(title_group, 0, 0, 1, 3)
+        grid.addLayout(welcome_layout, 1, 1)
+        grid.addWidget(date_label, 1, 2, 1, 1)
+        grid.addWidget(members_panel, 2, 0)
+        grid.addWidget(finance_panel, 2, 1)
+        grid.addWidget(stats_panel, 2, 2)
+        grid.addItem(self.spaceItem, 3, 0, 1, 3)
