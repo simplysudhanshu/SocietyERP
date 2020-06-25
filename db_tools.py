3fa7b27259fa7095ee8e07f7daad348d5a69f5d3
sudhanshu <sudhanshu.1.k@gmail.com>
2020-06-25 21:08:46 +0530

    Updated functionality with DB and populated DB

--- db_tools.py
+++ db_tools.py
@@ -38,7 +38,8 @@ def add_to_db(table: str, attributes: list):
         split_date = attributes[0].split("/")
         receipt_id = generate_receipt_id(month=split_date[1], year=split_date[2])
 
-        query = f"INSERT INTO records VALUES ('{receipt_id}', '{attributes[0]}', '{attributes[1]}', {attributes[2]}, '{attributes[3]}', '{attributes[4]}');"
+        query = f"INSERT INTO records VALUES ('{receipt_id}', '{attributes[0]}', '{attributes[1]}', '{attributes[2]}', " \
+                f"'{attributes[3]}', {attributes[4]}, {attributes[5]}, '{attributes[6]}', '{attributes[7]}');"
 
     try:
         conn.execute(query)
@@ -116,7 +117,7 @@ def delete_from_db(table: str, key: str = None, value=None):
         return True
 
 
-def get_receipts(date: str = None, flat: str = None, month: str = None):
+def get_statement(date: str = None, flat: str = None, month: str = None):
     conn = create_connection()
     output = []
     query = ''
@@ -126,11 +127,11 @@ def get_receipts(date: str = None, flat: str = None, month: str = None):
                 f"r.flat = m.flat AND r.date = '{date}';"
 
     elif flat is not None:
-        query = f"SELECT receipt_id, date, amount, mode FROM records WHERE flat = '{flat}';"
+        query = f"SELECT receipt_id, date, amount, mode, ref FROM records WHERE flat = '{flat}';"
 
     elif month is not None:
-        query = "SELECT r.receipt_id, r.date, r.flat, m.name, r.amount, r.mode FROM records r, members m WHERE " \
-                f"r.flat = m.flat AND date LIKE '%{month}/%';"
+        query = "SELECT r.receipt_id, r.date, r.flat, m.name, r.amount, r.mode, r.ref FROM records r, members m WHERE " \
+                f"r.flat = m.flat AND r.date LIKE '%/{month}/%';"
 
     try:
         cursor = conn.execute(query)
@@ -151,30 +152,55 @@ def get_receipts(date: str = None, flat: str = None, month: str = None):
 # connection.execute("CREATE TABLE members (flat TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL, current TEXT NOT NULL, "
 #                    "contact NUMBER NOT NULL, email TEXT NOT NULL);")
 
-# connection.execute("CREATE TABLE records (receipt_id TEXT PRIMARY KEY NOT NULL, date TEXT NOT NULL, flat TEXT NOT NULL, "
-#                    "amount REAL NOT NULL, mode TEXT NOT NULL, ref TEXT NOT NULL);")
+# connection.execute("CREATE TABLE records (receipt_id TEXT PRIMARY KEY NOT NULL, date TEXT NOT NULL, flat TEXT NOT NULL, fee_month TEXT NOT NULL, fee_till TEXT, "
+#                    "amount REAL NOT NULL, fine READ NOT NULL, mode TEXT NOT NULL, ref TEXT);")
 
 # connection.execute("DELETE FROM members;")
 # connection.execute("DELETE FROM records;")
 
-# conn.execute("DROP TABLE members;")
+# connection.execute("DROP TABLE members;")
+# connection.execute("DROP TABLE records;")
+
+# cursor1 = connection.execute("SELECT r.receipt_id, r.date, r.flat, m.name, r.amount, r.mode FROM records r, members m WHERE r.flat = m.flat AND r.date LIKE '%/06/%';")
 
 # connection.commit()
 
 # --
 
-# add_to_db(table='members', attributes=["A-03", "Mr Dhumal", "Mr Patil", 1, "a@xyz"])
-# add_to_db(table='records', attributes=["23/02/20", "A-02", 1500, "cash", "cash"])
+# add_to_db(table='members', attributes=["A-09", "Mr Kulkarni", "Mr Kulkarni", 1, "a@xyz"])
+# add_to_db(table='records', attributes=["23/06/20", "A-10", 1500, "cash", "cash"])
 
 # update_db(table="members", identifier="A-01", all_attributes=["Mr Patil", "Mr Patil", 1, "a@xyz"])
 
-# print(generate_receipt_id(month=6, year=20))
-# print(get_from_db(table="members", attribute="*", key="flat", value="A-01"))
+# print(generate_receipt_id(month='6'))
+# print(get_from_db(table="members", attribute="name", key="flat", value="A - 1"))
 
-# print(get_receipts(month="01"))
+# print(get_receipts(month="06"))
 
-# cursor1 = connection.execute("SELECT * FROM members';")
-# print(list(cursor1))
+# cursor1 = connection.execute("SELECT * FROM members;")
+# for row in cursor1:
+#     print(row)
 
 # cursor2 = connection.execute("SELECT * FROM records")
 # print(list(cursor2))
+
+
+'''
+Populating the database:
+
+names = ["Mr D. S. Patil", "Mr C. Y. Carvalho", "Mrs M. A. Kotangle", "Mr B. R. Varpe", "Mr V. A. Dhumal",
+         "Mr A. D. Kulkarni", "Mr V. G. Dhapre", "Mrs S. S. Pawar", "Mr P. S. Kulkarni", "Mr R. R. Tahilramani",
+         "Mr. S. G. Iyer", "Mr P. S. Babel", "Mr K. J. Sharma", "Mr M. N. Kanchan", "Mr B. B. Mankeshwarkar",
+         "Mr A. V. Shrivastava", "Mr K. R. Rathor", "Mr P. K. Das", "Mr V. A. Sutar", "Mr S. S. Chavan",
+         "Mr. A. P. Pandey", "Mr. A. N. Mujawar"]
+
+numbers = [9822599523, 0, 7208171228, 9850007465, 9822433024, 9850841850, 8208662242, 9764406444, 9850956132, 0, 9765401804,
+           9922420609, 8888405454, 9881995002, 9028318480, 9730014518, 9764975993, 9890453488, 0, 9881499697, 8888887300, 9890310831]
+
+email = "example@xyz"
+
+flats = [f"A - {str(x)}" for x in range(1, 23)]
+
+for index, flat in enumerate(flats):
+    add_to_db(table='members', attributes=[flat, names[index], names[index], numbers[index], email])
+'''
