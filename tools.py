107e56bd302f96d050aaf7bd9e25df82eb93b48d
sudhanshu <sudhanshu.1.k@gmail.com>
2020-07-05 11:31:26 +0530

    Authentication, backup generation, SMTP functions

--- tools.py
+++ tools.py
@@ -1,7 +1,18 @@
+import os
+import datetime
+import time
+import shutil
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
 from PyQt5.QtWidgets import QSpacerItem, QSizePolicy
 import matplotlib.pyplot as plt
 import matplotlib
-import datetime
+import pdfkit
+from jinja2 import FileSystemLoader, Environment
 
 import db_tools
 
@@ -20,6 +31,34 @@ flats = [f"A - {str(x)}" for x in range(1, 23)]
 
 currentMonth = datetime.datetime.now().month
 currentYear = datetime.datetime.now().year
+
+
+def authenticate():
+    if os.path.isfile("C:\\Users\\Public\\Public SocietyERP-Config\\97y04m13d.dat"):
+        return True
+    else:
+        return False
+
+
+def verify_code(code: str):
+    datefile = open("C:\\Users\\Public\\Public SocietyERP-Config\\97y04m13d.dat", "r+")
+    system_code = datefile.read()
+    system_code = system_code.split("#")
+
+    code_snippet = system_code[-1]
+
+    if code == code_snippet:
+        return True
+    else:
+        return False
+
+
+def create_secret(name: str, code: int, curr_date: str):
+    os.mkdir("C:\\Users\\Public\\Public SocietyERP-Config")
+    file = open("C:\\Users\\Public\\Public SocietyERP-Config\\97y04m13d.dat", "a")
+    # curr_date = time.strftime("%Y-%m-%d")                                                                             # FIX THIS BACK !
+
+    file.write(f"{curr_date}#{name}#{code}")
 
 
 def create_spacer_item(w: int, h: int):
@@ -253,6 +292,176 @@ def get_home_stats():
     plt.savefig("pie.png", bbox_inches='tight', pad_inches=0)
 
     return {"received": received, "pending": pending, "percent": percent, 'funds': funds}
+
+
+def payment_exists(flat: str, month: str):
+    member_data = db_tools.get_members_stats(one_member=flat)
+
+    check_month, check_year = month.split(" '")
+
+    for row in member_data:
+        if row[3] != "-":
+            row[2] = row[3]
+
+        row.remove(row[3])
+
+        row_month, row_year = row[2].split("/")
+
+        if int(row_year) < int(check_year):
+            return False
+
+        elif int(row_year) == int(check_year):
+            if int(get_month_number(row_month)) < int(get_month_number(check_month)):
+                return False
+
+            else:
+                return True
+
+
+def backup():
+    datefile = open("C:\\Users\\Public\\Public SocietyERP-Config\\97y04m13d.dat", "r+")
+    last_update = datefile.read()
+    last_update = last_update.split("#")
+
+    last_date = last_update[0]
+
+    current_date = time.strftime("%Y-%m-%d")
+
+    if last_date[2:4] != current_date[2:4] or last_date[5:7] != current_date[5:7]:
+
+        if last_date[2:4] != current_date[2:4]:
+            datefile.close()
+            shutil.rmtree(path="C:\\Users\\Public\\Public SocietyERP-Config")
+            os.mkdir("C:\\Users\\Public\\Public SocietyERP-Config")
+            datefile = open("C:\\Users\\Public\\Public SocietyERP-Config\\97y04m13d.dat", "a")
+
+        last_update[0] = current_date
+        new_update = ''.join([f"{x}#" for x in last_update])
+
+        datefile.write(new_update[:-1])
+        datefile.close()
+
+        backup_file = os.path.join('C:\\Users\\Public\\Public SocietyERP-Config',
+                                   os.path.basename('apartment') + time.strftime("%d-%m-%Y-%H%M"))
+
+        shutil.copyfile('apartment', backup_file)
+
+        members_file, records_file = db_tools.generate_csv()
+        send_backup(backup_path=backup_file, members_path=members_file, records_path=records_file, month=all_months[current_date.split("-")[1]])
+    else:
+        pass
+
+
+def send_receipt(receipt: str, flat: str, month: str):
+    other_data = db_tools.get_from_db(table='members', attribute='name, email_id', key='flat', value=flat)
+    other_name = other_data[0][0]
+    other_address = other_data[0][1]
+
+    receipt_file = open(receipt, 'rb')
+
+    my_address = ''
+    my_password = ''
+
+    mail_content = f"Hello {other_name},\n" \
+                   f"Thank you for paying the society maintenance charges for the month of {month}." \
+                   f"Please find attached, your receipt for the same.\n\n" \
+                   f"Regards,\n" \
+                   f"Shree Moraya Gosavi Raj Park - II"
+
+    message = MIMEMultipart()
+
+    message['From'] = my_address
+    message['To'] = other_address
+    message['Subject'] = f'Society Maintenance Receipt : {month.upper()}'
+    message.attach(MIMEText(mail_content, 'plain'))
+
+    payload_receipt = MIMEBase('application', 'octa-stream')
+    payload_receipt.set_payload(receipt_file.read())
+    encoders.encode_base64(payload_receipt)
+    payload_receipt.add_header('Content-Disposition', 'attachment', filename=f'receipt({month})')
+    message.attach(payload_receipt)
+
+    session = smtplib.SMTP(host="smtp.gmail.com", port=587)
+    session.ehlo()
+    session.starttls()
+    session.login(my_address, my_password)
+
+    session.send_message(message)
+    session.quit()
+
+
+def send_backup(backup_path: str, members_path: str, records_path: str, month: str):
+    backup_file = open(backup_path, 'rb')
+    members_file = open(members_path, 'rb')
+    records_file = open(records_path, 'rb')
+
+    my_address = ''
+    my_password = ''
+
+    mail_content = f"Monthly Backup Service for SocietyERP. \nBackup generated for the month of {month.upper()}."
+
+    message = MIMEMultipart()
+
+    message['From'] = my_address
+    message['To'] = my_address
+    message['Cc'] = 'pskulkarni1968@gmail.com'
+    message['Subject'] = f'Data Backup : {month.upper()}'
+    message.attach(MIMEText(mail_content, 'plain'))
+
+    payload_backup = MIMEBase('application', 'octa-stream')
+    payload_backup.set_payload(backup_file.read())
+    encoders.encode_base64(payload_backup)
+    payload_backup.add_header('Content-Disposition', 'attachment', filename=f'db_backup({month})')
+    message.attach(payload_backup)
+
+    payload_members = MIMEBase('application', 'octa-stream')
+    payload_members.set_payload(members_file.read())
+    encoders.encode_base64(payload_members)
+    payload_members.add_header('Content-Disposition', 'attachment', filename=f'members_backup({month})')
+    message.attach(payload_members)
+
+    payload_records = MIMEBase('application', 'octa-stream')
+    payload_records.set_payload(records_file.read())
+    encoders.encode_base64(payload_records)
+    payload_records.add_header('Content-Disposition', 'attachment', filename=f'records_backup({month})')
+    message.attach(payload_records)
+
+    session = smtplib.SMTP(host="smtp.gmail.com", port=587)
+    session.starttls()
+    session.login(my_address, my_password)
+
+    session.send_message(message)
+    session.quit()
+
+
+def design_receipt():
+    loader = FileSystemLoader('template')
+    env = Environment(loader=loader)
+    template = env.get_template(name='receipt_template.html')
+    return template.render(data="Using JINJA2 templating")
+
+
+def receipt_to_pdf(flat: str, month: str, input_text: str):
+    user = os.environ['USERPROFILE']
+    path = user + '\\Desktop\\SocietyERP'
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    pdf_path = f'{path}\\receipt_{month}({flat}).pdf'
+
+    path_wkhtmltopdf = r'wkhtmltopdf/bin/wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    options = {'quiet': ''}
+
+    pdfkit.from_string(input=input_text, output_path=pdf_path, configuration=config, options=options)
+
+
+# receipt_data = design_receipt()
+# receipt_to_pdf(flat='A - 9', month='January', input_text=receipt_data)
+# receipt_to_pdf(flat='A - 9', month='July')
+# create_secret(name="Sudhanshu", code=2513, curr_date="2019-06-03")
+# backup()
+# print(str(datetime.datetime.now()).split(" ")[0])
 
 # calculate_fine(month="May '20", transact_date="06/01/21")
 
